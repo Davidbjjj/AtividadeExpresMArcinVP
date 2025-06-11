609484cd989826e43fce80d0aff2797b74a72f0e
davidbjjj <pondavid103@gmail.com>
2025-06-11 20:29:32 -0300

    alteração

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const AlunoController = require('../controllers/AlunoController');
+const AlunoController = require('../controllers/alunoController');
 
 router.get('/', AlunoController.listarTodos);
 router.post('/', AlunoController.criar);
