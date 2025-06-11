3f4b39724e1369f7494cf2f78dd1c75f189d2b6f
davidbjjj <pondavid103@gmail.com>
2025-06-11 03:03:13 -0300

    Alteração PROD

--- src/routes/disciplinaRoutes.js
+++ src/routes/disciplinaRoutes.js
@@ -9,16 +9,16 @@ router.get('/:id', disciplinaController.buscarPorId);
 router.put('/:id', disciplinaController.atualizarDisciplina);
 router.delete('/:id', disciplinaController.deletarDisciplina);
 
-// // Matrícula de alunos
-// router.post('/:disciplinaId/alunos/:alunoId', disciplinaController.matricularAluno);
-// router.delete('/:disciplinaId/alunos/:alunoId', disciplinaController.desmatricularAluno);
-// router.get('/:disciplinaId/alunos', disciplinaController.listarAlunos);
+// Matrícula de alunos
+router.post('/:disciplinaId/alunos/:alunoId', disciplinaController.matricularAluno);
+router.delete('/:disciplinaId/alunos/:alunoId', disciplinaController.desmatricularAluno);
+router.get('/:disciplinaId/alunos', disciplinaController.listarAlunos);
 
-// // Notificações
-// router.post('/:disciplinaId/notificar', disciplinaController.notificarAlunos);
+// Notificações
+router.post('/:disciplinaId/notificar', disciplinaController.notificarAlunos);
 
-// // Consultas específicas
-// router.get('/escola/nome/:nome', disciplinaController.listarPorNomeEscola);
-// router.post('/:nomeDisciplina/associar-professor/:emailProfessor', disciplinaController.associarProfessor);
+// Consultas específicas
+router.get('/escola/nome/:nome', disciplinaController.listarPorNomeEscola);
+router.post('/:nomeDisciplina/associar-professor/:emailProfessor', disciplinaController.associarProfessor);
 
 module.exports = router;
